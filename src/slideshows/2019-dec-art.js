06a4eb894f8ad6268538f381f238cd411830ef49
maxhudson <max@henrybuilt.com>
2019-12-01 14:27:13 -0800

    art

---
+++ src/slideshows/2019-dec-art.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+var slideshow = {
+  id: 3,
+  title: '?',
+  author: 'Max',
+  path: '2019-dec-art',
+  slides: [
+    {
+      layout: 'TlMr',
+      text: {title: 'Communication', body: `manipulation\n\nvs.\n\nshared awareness`},
+      media: [{type: 'image', filename: 'bird-52.png'}]
+    },
+    {
+      layout: 'TrMl',
+      text: {title: 'Drip. Drip.', body: `Slow at first
+
+It grows
+Of pure light
+Yellow coals
+
+Then a leaky faucet
+On and off
+But always slowly dripping`},
+      media: [{type: 'image', filename: 'helen.png'}]
+    }
+  ]
+};
+
+export default slideshow;
