839901eb8b1954b9f8b03ce1d34b8b181076a8a5
maxhudson <max@henrybuilt.com>
2019-12-23 17:49:15 -0800

    fixed error

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -81,12 +81,10 @@ class Slideshow extends Component {
     this.setState({id: _.get(this.props, 'match.params.id')});
 
     document.addEventListener('keydown', this.handleKeyDown);
-    document.addEventListener('keydown', this.handleWindowScroll);
   }
 
-  componentDidUnmount() {
+  componentWillUnmount() {
     document.removeEventListener('keydown', this.handleKeyDown);
-    document.removeEventListener('keydown', this.handleWindowScroll);
   }
 
   componentDidUpdate(prevProps) {
@@ -109,25 +107,19 @@ class Slideshow extends Component {
 
   toggleIsFullscreen = () => this.setState(({isFullscreen}) => ({isFullscreen: !isFullscreen}));
 
-  next = (event) => this.setIndex(this.state.index + 1, event);
-  prev = (event) => this.setIndex(this.state.index - 1, event);
+  next = () => this.setIndex(this.state.index + 1);
+  prev = () => this.setIndex(this.state.index - 1);
 
-  setIndex = (index, event) => {
+  setIndex = (index) => {
     var {slides} = this.slideshow;
     var count = slides.length;
 
     this.setState({index: (index + count) % count});
-
-    if (event) event.preventDefault();
   }
 
   handleKeyDown = (event) => {
-    if (event.keyCode === 39) this.next(event);
-    if (event.keyCode === 37) this.prev(event);
-  }
-
-  handleWindowScroll = (event) => {
-    event.preventDefault();
+    if (event.keyCode === 39) this.next();
+    if (event.keyCode === 37) this.prev();
   }
 
   get slideshow() {
@@ -143,8 +135,8 @@ class Slideshow extends Component {
     return (
       <Swipeable
         {...className(['slideshow', this.state.isTransitioning && '--is-transitioning'])}
-        onSwipedLeft={this.next}
-        onSwipedRight={this.prev}
+        // onSwipedLeft={this.next}
+        // onSwipedRight={this.prev}
         delta={3}
       >
         <div className='preload'>
