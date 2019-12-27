b84853028e1861a07659f84c4074ebc8ba68be4a
maxhudson <max@henrybuilt.com>
2019-12-27 09:55:46 -0800

    chores

--- src/slideshows/9-chores.js
+++ src/slideshows/9-chores.js
@@ -8,12 +8,12 @@ var slideshow = {
   slides: [
     {
       layout: 'TrMl',
-      text: {title: 'Square One', body: `Always reset.\n\nIf I don't clean appliances, sinks, and other weird, gross things to the point where they look like new, they'll keep getting worse.`},
+      text: {title: 'Square One', body: `If I don't restore appliances, sinks, and other gross things to "like new" occasionally, they'll become irreversibly gunky before long.\n\nFind a way to get rid of the fingerprints, the stuff in the cracks, the thin film of semi-permanent grime.`},
       media: [{type: 'image', filename: 'chores-1.jpg'}]
     },
     {
       layout: 'TlMr',
-      text: {title: 'Dishes', body: `Doing someone else's dishes feels better than doing my own. \n\nIf I do someone else's dishes, they might do some of mine.\n\nI give and receive the gift of clean dishes for the same amount of work as trudging through my own dishes and avoiding someone else's.`},
+      text: {title: 'Dishes', body: `Doing someone else's dishes feels better than doing my own.\n\nIf I do someone else's dishes, they might do some of mine.\n\nI can give and get clean dishes - or trudge through my own and avoid others - for the same amount of work.\n\nJust don't expect anything.`},
       media: [{type: 'image', filename: 'chores-2.jpg'}]
     },
     {
